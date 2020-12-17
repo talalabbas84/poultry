4b55ab3655e14830942fff39eb49ac960b7c10a1
Sheryar Shirazi <sheryar@at-event.com>
2020-12-18 01:48:07 +0500

    login screen added

---
+++ src/screens/login/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import AppStyles from '../../styles';
+import Button from '../../components/button';
+import TextInput from '../../components/textInput';
+import colors from '../../constants/colors';
+import {phone, logo, user} from '../../constants/images';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: colors.backColor}}>
+        <Image style={AppStyles.imageLogo} source={logo} />
+        <View style={AppStyles.line} />
+        <Text style={AppStyles.boldText}>Login</Text>
+
+        <TextInput length={11} image={user} placeholder="Full Name" />
+
+        <TextInput
+          length={11}
+          keyboardType={'numeric'}
+          image={user}
+          placeholder="Phone Number"
+        />
+
+        <Button
+          onPress={() => this.props.navigation.navigate('DrawerStack')}
+          red
+          title="COMPLETE LOGIN"
+        />
+      </View>
+    );
+  }
+}
+export default Login;
